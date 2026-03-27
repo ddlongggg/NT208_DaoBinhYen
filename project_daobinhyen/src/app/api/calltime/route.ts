1f81a6e81d0cb2d79df83a54261b90b1ff5385b9
24520127-TongKhiDucAnh <24520127@gm.uit.edu.vn>
2026-03-27 18:33:46 +0700

    Update route.ts

--- project_daobinhyen/src/app/api/calltime/route.ts
+++ project_daobinhyen/src/app/api/calltime/route.ts
@@ -3,14 +3,14 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   try {
     // Gọi đến một API thời gian miễn phí và cực chuẩn trên thế giới
-    const response = await fetch('http://worldtimeapi.org/api/timezone/Asia/Ho_Chi_Minh', {
+    const response = await fetch('https://timeapi.io/api/Time/current/zone?timeZone=Asia/Ho_Chi_Minh', {
       cache: 'no-store'
     });
     
     const data = await response.json();
     
-    // data.datetime sẽ có dạng "2026-03-27T18:05:00.123456+07:00"
-    const standardTime = new Date(data.datetime).toLocaleString('vi-VN');
+    // data.dateTime sẽ có dạng "{"localTime":"18:29:53 27/3/2026"}"
+    const standardTime = new Date(data.dateTime).toLocaleString('vi-VN');
 
     return NextResponse.json({ 
       localTime: standardTime
